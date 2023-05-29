a18bc9a602b6c5f313c9c5dcd8478b0019803640
Claudio Torcato <claudiotorcato@gmail.com>
2023-05-28 23:46:09 -0300

    movendo arquivos Python para pacote gehennabot

---
+++ gehennabot/cli.py
@@ -0,0 +1,14 @@
+def listar_preconstruidos():
+    pass
+
+
+def listar_usuarios():
+    pass
+
+
+def preconstruido_como_entrada(deck_id, dono_id):
+    pass
+
+
+def preconstruido_como_saida(deck_id, dono_id):
+    pass
